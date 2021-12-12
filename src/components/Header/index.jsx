c0290ea0c9fd67e9b761c3fc39f7a924d0c52a0b
Yahya Hafez <yhafez3@gmail.com>
2021-12-12 14:45:22 -0600

    Added header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Super marketeer</h1>
+            <h2>A supermarket inventory management system</h2>
+        </header>
+    );
+}
