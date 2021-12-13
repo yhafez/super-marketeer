8e82e1dc12b2007f44e010be7a1ed7bd203fe56b
Yahya Hafez <yhafez3@gmail.com>
2021-12-12 18:43:58 -0600

    Configured delete input field

--- src/components/Cards/DeleteCard/index.jsx
+++ src/components/Cards/DeleteCard/index.jsx
@@ -4,6 +4,43 @@ import "./DeleteCard.css";
 export default function DeleteCard() {
     const [productCode, setProductCode] = useState("");
 
+    const handleInput = (value) => {
+        const alphaNumericCheck = /^\s*([0-9a-zA-Z-]*)\s*$/;
+        if (!value.match(alphaNumericCheck))
+            return alert("no special characters allowed");
+
+        const length = value.length;
+
+        // Remove dash up to 3 times
+        if (value.includes("-")) {
+            value = value.replace("-", "");
+            value = value.replace("-", "");
+            value = value.replace("-", "");
+        }
+
+        if (length <= 4) setProductCode(value);
+        else if (length > 3 && length < 10) {
+            value = value.slice(0, 4) + "-" + value.slice(4);
+        } else if (length > 9 && length < 15) {
+            value =
+                value.slice(0, 4) +
+                "-" +
+                value.slice(4, 8) +
+                "-" +
+                value.slice(8);
+        } else if (length > 14 && length < 20) {
+            value =
+                value.slice(0, 4) +
+                "-" +
+                value.slice(4, 8) +
+                "-" +
+                value.slice(8, 12) +
+                "-" +
+                value.slice(12);
+        }
+        setProductCode(value);
+    };
+
     return (
         <div className="card-container">
             <h3 className="card-title">Delete a Product</h3>
@@ -13,8 +50,8 @@ export default function DeleteCard() {
                     type="text"
                     placeholder="Product Code"
                     value={productCode}
-                    maxLength="20"
-                    onChange={(e) => setProductCode(e.target.value)}
+                    maxLength="19"
+                    onChange={(e) => handleInput(e.target.value)}
                 />
                 <input
                     className="submit-button"
